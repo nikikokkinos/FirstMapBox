4a833989d584f690af35f5dfff28e4ecd2eec177
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2019-05-09 14:46:53 -0400

    added GR_Regions geojson layer

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,33 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibmlraTEyc3RlcCIsImEiOiJjanZlNGFneWswMm0zNDRxc
 
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/niki12step/cjvfptvqghlsn1fs3e3lmh40l',
+  style: 'mapbox://styles/mapbox/streets-v11',
   center: [23.389893, 38.298559],
   zoom: 5.6,
   minZoom: 3,
 });
 
-// map.addSource('regions', {
-//   background-color:
-// });
+map.on('load', function() {
+  var url = 'https://raw.githubusercontent.com/nikikokkinos/GR_Data/master/GR_Regions.geojson';
+  map.addSource('regions', {
+    type: 'geojson',
+    data: url,
+  });
 
-// map.on("load", function() {
-//   map.addLayer("regions")});
+  map.addLayer({
+  'id': 'regionsLayer',
+  'type': 'fill',    /*define the type of layer fill, line, point, fill-extrusion, background, raster, circle*/
+  'source': 'regions',
+  'layout': {
+    'visibility': 'visible'
+  },
+
+  /*there are many options for styling - this is a simple style*/
+
+  'paint': {
+    'fill-color': 'skyblue',
+    'fill-outline-color': 'white',
+    'fill-opacity': 0.9
+  }
+});
+});
